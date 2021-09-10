5d1ea15200d0adf457c211d0999e8c56d2443778
dangkh <Dangkh109@gmail.com>
2021-09-10 16:27:56 +0700

    add code triplet

--- nets.py
+++ nets.py
@@ -2,6 +2,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+import numpy as np
+import torch.optim as optim
+from torch.optim import lr_scheduler
+
+
+h1, w1 = 3, 1
+h2, w2 = 3, 3
+h3, w3 = 3, 5
+width = 10
+height = 10
+paramsCNN2D = {'conv_channels': [
+            [1, 16, 8],
+            [1, 16, 32],
+            [1, 64, 32, 16, 8],
+            [1, 128, 64, 32, 16, 8],
+            [1, 256, 128, 64, 32, 16, 8]
+        ],
+            'kernel_size': [[(h1, w1 * width), (h1, w1 * width), (h1, w1 * width),
+                             (h1, w1 * width), (h1, w1 * width), (h1, w1 * width)],
+
+                            [(h2 * height, w2), (h2 * height, w2), (h2 * height, w2),
+                             (h2 * height, w2), (h2 * height, w2), (h2 * height, w2)],
+
+                            [(h3, w3 * width), (h3, w3 * width), (h3, w3 * width),
+                             (h3, w3 * width), (h3, w3 * width), (h3, w3 * width)]]
+        }
+
+
 class LSTMNet(nn.Module):
     def __init__(self, n_classes):
         super(LSTMNet, self).__init__()
@@ -36,3 +64,189 @@ class LSTMNet(nn.Module):
         output = self.drop1(output)
         output = self.fc1(output)
         return output
+
+
+class CNN_LSTM(torch.nn.Module):
+    def __init__(self, input_size, kernel_size, conv_channels,
+                 dense_size, dropout, nclass = 3):
+        super(CNN_LSTM, self).__init__()
+        self.cconv = []
+        self.MaxPool = nn.MaxPool2d((1, 2), (1, 2))
+        self.ReLU = nn.ReLU()
+        self.Dropout = nn.Dropout(dropout)
+        self.batchnorm = []
+        self.bn = nn.BatchNorm2d(32, momentum=0.1, affine=True)
+        # ############ batchnorm ###########
+        for jj in conv_channels:
+            self.batchnorm.append(nn.BatchNorm2d(jj, eps=0.001, momentum=0.01,
+                                                 affine=True))
+        ii = 0  ##### define CONV layer architecture: #####
+        for in_channels, out_channels in zip(conv_channels, conv_channels[1:]):
+            conv_i = torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                     kernel_size=kernel_size[ii],  stride = (2, 1),
+                                     padding=(kernel_size[ii][0] // 2, kernel_size[ii][1] // 2))
+            self.cconv.append(conv_i)
+            self.add_module('CNN_K{}_O{}'.format(kernel_size[ii], out_channels), conv_i)
+            ii += 1
+
+        output_LSTM = self.get_output_LSTMdim(input_size, self.cconv)
+        numFeatures = output_LSTM[-1]
+        self.lstm = nn.LSTM(numFeatures, 8, bidirectional=True, batch_first=True)
+        self.flat_dim = self.get_output_dim(input_size, self.cconv)
+        self.fc1 = torch.nn.Linear(self.flat_dim, dense_size)
+        self.fc2 = torch.nn.Linear(dense_size, nclass)
+
+    def get_output_LSTMdim(self, input_size, cconv):
+        with torch.no_grad():
+            input = torch.ones(1, *input_size)
+            for conv_i in cconv:
+                input = self.MaxPool(conv_i(input))
+                flatout = int(np.prod(input.size()[1:]))
+        shape = input.shape
+        output = input.view(len(input), shape[1] * shape[2] ,-1)
+        return output.shape
+
+    def get_output_dim(self, input_size, cconv):
+        with torch.no_grad():
+            input = torch.ones(1, *input_size)
+            for conv_i in cconv:
+                input = self.MaxPool(conv_i(input))
+                flatout = int(np.prod(input.size()[1:]))
+                print("Input shape : {} and flattened : {}".format(input.shape, flatout))
+        shape = input.shape
+        output = input.view(len(input), shape[1] * shape[2] ,-1)
+        print("LSTM input: ", output.shape)
+        h_lstm, _ = self.lstm(output)
+        print("output LSTM: ", output.shape)
+        output = torch.flatten(h_lstm,1)
+        return output.shape[-1]
+
+    def forward(self, x):
+        for jj, conv_i in enumerate(self.cconv):
+            # print(jj)
+            x = conv_i(x)
+            if jj == len(self.cconv) - 1:
+                x = self.bn(x)
+            x = self.ReLU(x)
+            x = self.MaxPool(x)
+        shape = x.shape
+        output = x.view(len(x), shape[1] * shape[2] ,-1)
+        h_lstm, _ = self.lstm(output)
+        output = torch.flatten(h_lstm,1)
+        output = F.relu(self.fc1(output))
+        output = self.Dropout(output)
+        output = self.fc2(output)
+        return output
+
+class TripletNet(nn.Module):
+    def __init__(self, baseNet):
+        super().__init__()
+        self.embedding_net = baseNet
+
+    def forward(self, x1):
+        output1 = self.embedding_net(x1)
+        return output1
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
+
+# Compute the distance matrix
+# https://omoindrot.github.io/triplet-loss
+# numpy + torch version
+def _pairwise_distances(embeddings, squared=False):
+    """Compute the 2D matrix of distances between all the embeddings.
+
+    Args:
+        embeddings: tensor of shape (batch_size, embed_dim)
+        squared: Boolean. If true, output is the pairwise squared euclidean distance matrix.
+                 If false, output is the pairwise euclidean distance matrix.
+
+    Returns:
+        pairwise_distances: tensor of shape (batch_size, batch_size)
+    """
+    # Get the dot product between all embeddings
+    # shape (batch_size, batch_size)
+    dot_product = torch.matmul(embeddings, embeddings.T)
+
+    # Get squared L2 norm for each embedding. We can just take the diagonal of `dot_product`.
+    # This also provides more numerical stability (the diagonal of the result will be exactly 0).
+    # shape (batch_size,)
+    square_norm = torch.diag(dot_product)
+
+    # Compute the pairwise distance matrix as we have:
+    # ||a - b||^2 = ||a||^2  - 2 <a, b> + ||b||^2
+    # shape (batch_size, batch_size)
+    distances = torch.unsqueeze(square_norm, 0) - 2.0 * dot_product + torch.unsqueeze(square_norm, 1)
+
+    # Because of computation errors, some distances might be negative so we put everything >= 0.0
+    distances = torch.maximum(distances, torch.tensor(0.0))
+
+    if not squared:
+        # # Because the gradient of sqrt is infinite when distances == 0.0 (ex: on the diagonal)
+        # # we need to add a small epsilon where distances == 0.0
+        # mask = tf.to_float(tf.equal(distances, 0.0))
+        # distances = distances + mask * 1e-16
+
+        distances = torch.sqrt(distances)
+
+        # # Correct the epsilon added: set the distances on the mask to be exactly 0.0
+        # distances = distances * (1.0 - mask)
+
+    return distances
+
+def _get_triplet_mask(labels):
+    # (where label(a) != label(p) or label(n) == label(a) or a == p)
+    nn = len(labels)
+    mask = torch.ones([nn, nn, nn])
+    for a in range(nn):
+        for p in range(nn):
+            for n in range(nn):
+                if labels[a] != labels[p] or labels[n] == labels[a] or a == p:
+                    mask[a, p, n] = 0
+    return mask
+
+# Compute the distance matrix
+# https://omoindrot.github.io/triplet-loss
+# numpy + torch version
+
+def batch_all_triplet_loss(labels, embeddings, margin, squared=False):
+    """Build the triplet loss over a batch of embeddings.
+
+    We generate all the valid triplets and average the loss over the positive ones.
+
+    Args:
+        labels: labels of the batch, of size (batch_size,)
+        embeddings: tensor of shape (batch_size, embed_dim)
+        margin: margin for triplet loss
+        squared: Boolean. If true, output is the pairwise squared euclidean distance matrix.
+                 If false, output is the pairwise euclidean distance matrix.
+
+    Returns:
+        triplet_loss: scalar tensor containing the triplet loss
+    """
+    # Get the pairwise distance matrix
+    pairwise_dist = _pairwise_distances(embeddings, squared=squared)
+    anchor_positive_dist = torch.unsqueeze(pairwise_dist, 2)
+    anchor_negative_dist = torch.unsqueeze(pairwise_dist, 1)
+
+    # Compute a 3D tensor of size (batch_size, batch_size, batch_size)
+    # triplet_loss[i, j, k] will contain the triplet loss of anchor=i, positive=j, negative=k
+    # Uses broadcasting where the 1st argument has shape (batch_size, batch_size, 1)
+    # and the 2nd (batch_size, 1, batch_size)
+    triplet_loss = anchor_positive_dist - anchor_negative_dist + margin
+    # Put to zero the invalid triplets
+    # (where label(a) != label(p) or label(n) == label(a) or a == p)
+    mask = torch.as_tensor(_get_triplet_mask(labels)).requires_grad_(False).to(device)
+    triplet_loss = torch.mul(mask, triplet_loss)
+
+    # Remove negative losses (i.e. the easy triplets)
+    triplet_loss = torch.maximum(triplet_loss, torch.tensor(0.0))
+    # Count number of positive triplets (where triplet_loss > 0)
+    valid_triplets = torch.gt(triplet_loss, 1e-16)
+    num_positive_triplets = torch.sum(valid_triplets)
+    num_valid_triplets = torch.sum(mask)
+    fraction_positive_triplets = num_positive_triplets / (num_valid_triplets + 1e-16)
+
+    # Get final mean triplet loss over the positive valid triplets
+    triplet_loss = torch.sum(triplet_loss) / (num_positive_triplets + 1e-16)
+    return triplet_loss, fraction_positive_triplets
--- ultis.py
+++ ultis.py
@@ -4,54 +4,9 @@ import torch
 import os
 import numpy as np
 
-class EEG_data(Dataset):
-    """EEG_data set"""
-    
-    def __init__(self, datas, targets, transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=(0.5,), std=(0.5,))]), train = True):
-        self.y = targets
-        self.X = datas
-        self.transform = transforms
+from sklearn.model_selection import train_test_split
 
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, idx):
-        if self.y is not None:
-            return self.transform(self.X[idx]), self.y[idx]
-        else:
-            return self.transform(self.X[idx])
-
-colors = ['red', 'green', 'blue', 'black',
-              'yellow', 'pink', 'orange', 'brown',
-              'purple', 'charcoal', 'navy', 'emerald', 'turquoise']
-
-def plot_embeddings(embeddings, targets, xlim=None, ylim=None):
-    plt.figure(figsize=(10,10))
-    tmp = np.unique(targets)
-    counter = 0
-    for idx in tmp:
-        inds = np.where(targets==idx)[0]
-        for id in inds:
-            plt.scatter(embeddings[id][0][1], embeddings[id][1][2], alpha=0.5, color=colors[counter])
-        counter += 1
-    if xlim:
-        plt.xlim(xlim[0], xlim[1])
-    if ylim:
-        plt.ylim(ylim[0], ylim[1])
-    plt.legend(tmp)
-
-def checkSubFolder(path):
-    list_sub = []
-    subs = os.listdir(path)
-    for sub in subs:
-        subPath = path + '/'+ sub
-        infoPath = path + '/' + sub + '/info.json'
-        if os.path.isfile(infoPath) and os.path.isdir(subPath):
-            list_sub.append(subPath)
-    return list_sub
-
-
-def chunk(matrix, step_size = 128, window_size = 128):
+def chunk(matrix, step_size=128, window_size=128):
     list_matrix = []
     l, r = 0, window_size - 1
     while r <= matrix.shape[0]:
@@ -66,62 +21,77 @@ def chunk(matrix, step_size = 128, window_size = 128):
     return list_matrix
 
 
-def chunk_matrix(list_data, list_target, step_size = 32, window_size = 128):
+def chunk_matrix(list_data, list_target, list_test, step_size=128, window_size=128):
     list_matries = []
     list_ys = []
+    list_t = []
     for idx, matrix in enumerate(list_data):
         matries = chunk(matrix)
         list_matries.extend(matries)
-        y_matries = [list_target[idx].astype(int)] * len(matries)
+        y_matries = [list_target[idx]] * len(matries)
         list_ys.extend(y_matries)
+        test_matries = [list_test[idx]] * len(matries)
+        list_t.extend(test_matries)
 
-    return list_matries, list_ys
+    return list_matries, list_ys, list_t
 
-def addNoise(data, target):
+
+def addNoise(data, target, test):
     list_newdata = []
     list_newtarget = []
+    list_newtest = []
+    tmp = np.asarray(target)
+    mean = np.mean(tmp)
     for idx in range(len(data)):
-        tmpTarget = [0]*12
+        tmpTarget = [0] * 12
         matrix = np.copy(data[idx])
-        noise = np.random.normal(0, 0.05, size= matrix.shape)
+        noise = np.random.normal(mean, 0.1, size=matrix.shape)
         newmatrix = matrix + noise
         newmatrix = newmatrix.astype(np.double)
         list_newdata.append(newmatrix)
         # tmpTarget[target[idx]] = 1
         list_newtarget.append(target[idx])
-    return list_newdata, list_newtarget
+        list_newtest.append(test[idx])
+    return list_newdata, list_newtarget, list_newtest
 
-def randomRemoveSample(data, target):
+
+def randomRemoveSample(data, target, test):
     list_newdata = []
     list_newtarget = []
+    list_newtest = []
     for idx in range(len(data)):
         matrix = np.copy(data[idx])
-        numRandom = 15
+        lenRandom = matrix.shape[0]
+        numRandom = int(lenRandom / 10)
         listFrame = []
         for id in range(numRandom):
-          tmp = np.random.randint(1,128)
-          listFrame.append(tmp)
+            tmp = np.random.randint(1, matrix.shape[0])
+            listFrame.append(tmp)
         for f in listFrame:
-          if f > 1 and f < 126:
-            matrix[f] = matrix[f-1] + matrix[f+1] / 2
+            if f > 1 and f < lenRandom - 1:
+                matrix[f] = matrix[f - 1] + matrix[f + 1] / 2
         # print(matrix.shape)
         list_newdata.append(matrix)
         list_newtarget.append(target[idx])
-    return list_newdata, list_newtarget
+        list_newtest.append(test[idx])
+    return list_newdata, list_newtarget, list_newtest
 
-def randomSwapSample(data, target):
+
+def randomSwapSample(data, target, test):
     list_newdata = []
     list_newtarget = []
+    list_newtest = []
     for idx in range(len(data)):
         matrix = np.copy(data[idx])
+        lenRandom = matrix.shape[0]
         numRandom = 8
         listFrame = []
         for id in range(numRandom):
-            tmp = np.random.randint(1,128)
+            tmp = np.random.randint(3, lenRandom - 10)
             listFrame.append(tmp)
         listFrame = np.sort(listFrame)
-        for idy, v in  enumerate(listFrame):
-            if idy > 0 and listFrame[idy] < listFrame[idy-1]:
+        for idy, v in enumerate(listFrame):
+            if idy > 0 and listFrame[idy] < listFrame[idy - 1]:
                 listFrame[idy] += 1
 
         list_Matrix = []
@@ -138,9 +108,8 @@ def randomSwapSample(data, target):
         for x in range(4):
             swapMatrix.append(np.copy(list_Matrix[arr[x]]))
 
-
         listFrame = np.insert(listFrame, 0, 0)
-        listFrame = np.append(listFrame ,127)
+        listFrame = np.append(listFrame, lenRandom)
         list_Matrix = []
         for x in range(5):
             l = x * 2
@@ -150,7 +119,6 @@ def randomSwapSample(data, target):
             tmpMatrix = np.copy(matrix[dl:dr])
             list_Matrix.append(tmpMatrix)
 
-
         finalList = []
         for x in range(4):
             finalList.append(list_Matrix[x])
@@ -158,9 +126,128 @@ def randomSwapSample(data, target):
         finalList.append(list_Matrix[-1])
 
         finalMatrix = np.vstack(finalList)
-        # print(finalMatrix.shape)
-        # if finalMatrix.shape[0] == 128:
-        #   print(listFrame)
+
         list_newdata.append(finalMatrix)
         list_newtarget.append(target[idx])
-    return list_newdata, list_newtarget
+        list_newtest.append(test[idx])
+    return list_newdata, list_newtarget, list_newtest
+
+
+def augmentData(Xs, Ys, Ts, labels = None):
+    newXs = []
+    newYs = []
+    newTs = []
+    augmentLabel = labels
+    if labels is None:
+        augmentLabel = np.unique(Ys)
+    for label in augmentLabel:
+        X_source = Xs[np.where(Ys == label)]
+        y_source = Ys[np.where(Ys == label)]
+        t_source = Ts[np.where(Ys == label)]
+        datanoise, targetnoise, testnoise = addNoise(X_source, y_source, t_source)
+        dataRemove, targetRemove, testRemove = randomRemoveSample(X_source, y_source, t_source)
+        dataSwap, targetSwap, testSwap = randomSwapSample(X_source, y_source, t_source)
+        newXs.extend(datanoise)
+        newXs.extend(dataRemove)
+        newXs.extend(dataSwap)
+        newYs.extend(targetnoise)
+        newYs.extend(targetRemove)
+        newYs.extend(targetSwap)
+        newTs.extend(testnoise)
+        newTs.extend(testRemove)
+        newTs.extend(testSwap)
+    newXs.extend(Xs)
+    newYs.extend(Ys)
+    newTs.extend(Ts)
+    return np.asarray(newXs), np.asarray(newYs), np.asarray(newTs)
+
+
+def preprocessData(data, augment = True):
+    # augment data & chunk matrix & relabel
+    # datas contains data, targets contains label, data_indexes contains 
+    # scenario index of phase index
+    targets = []
+    datas = []
+    data_indexes = []
+    for idx, _ in enumerate(data):
+        info = data[idx]
+        targets.append(info[0])
+        datas.append(info[1])
+        data_indexes.append(info[2])
+    newdata, newtarget, newtest = chunk_matrix(datas, targets, data_indexes)
+    datas = np.asarray(newdata)
+    targets = np.asarray(newtarget)
+    data_indexes = np.asarray(newtest)
+    if augment:
+        newdata, newtarget, newtest = augmentData(datas, targets, data_indexes)
+        datas = np.asarray(newdata)
+        targets = np.asarray(newtarget)
+        data_indexes = np.asarray(newtest)
+
+    # relabel
+    (unique, counts) = np.unique(np.asarray(targets), return_counts=True)
+    frequencies = np.asarray((unique, counts)).T
+    print(frequencies)
+
+    # missing at some label, relabel from 1 - number of unique label
+    counter = 0
+    list_label = [x for x in range(len(frequencies))]
+    newtargets = np.copy(targets)
+    for x in range(len(frequencies)):
+        newtargets[np.where(targets == unique[x])] = x
+    targets = newtargets
+    print("new targets")
+    (unique, counts) = np.unique(np.asarray(targets), return_counts=True)
+    frequencies = np.asarray((unique, counts)).T
+    print(frequencies)
+    return datas, targets, data_indexes
+
+class EEG_data(Dataset):
+    def __init__(self, datas, targets=None,
+                 train=True):
+
+        self.y = targets
+        mean = np.mean(datas, axis=3, keepdims=True)
+        std = np.std(datas, axis=3, keepdims=True)
+        self.X = (datas - mean) / std
+        self.X = self.X.astype(np.float32)
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        if self.y is not None:
+            return torch.tensor(self.X[idx]), self.y[idx]
+        else:
+            return torch.tensor(self.X[idx])
+
+def TrainTestLoader(data, testSize=0.1):
+    if len(data) == 2:
+        X_train, X_test, y_train, y_test = train_test_split(data[0], data[1], test_size=testSize, random_state=42)
+    else:
+        [X_train, y_train, X_test, y_test] = data
+    batch_size = 32
+
+    train_dataset = EEG_data(X_train, y_train)
+    test_dataset = EEG_data(X_test, y_test)
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size)
+
+    return train_loader, test_loader
+
+def trainTestSplit(data, target, test, testIndex):
+    X_train = []
+    X_test= []
+    y_train = []
+    y_test = []
+    for idx in range(len(data)):
+        if test[idx] == testIndex:
+            X_test.append(data[idx])
+            y_test.append(target[idx])
+        else:
+            X_train.append(data[idx])
+            y_train.append(target[idx])
+    return X_train, X_test, y_train, y_test
+        
